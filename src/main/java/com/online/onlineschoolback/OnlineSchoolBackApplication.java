c0c311426140bfc19030bfd6b40516916149da2c
周振新 <zhouzhenxin@oneweone.cn>
2022-08-12 16:15:53 +0800

    创建项目

---
+++ src/main/java/com/online/onlineschoolback/OnlineSchoolBackApplication.java
@@ -0,0 +1,13 @@
+package com.online.onlineschoolback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSchoolBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineSchoolBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/online/onlineschoolback/OnlineSchoolBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.onlineschoolback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSchoolBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
